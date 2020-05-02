9b8ef854516cbfac6cc1cfb1f6c256d2006c1592
SOFe <sofe2038@gmail.com>
2020-05-02 13:46:04 +0800

    Updated shields

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # serde-iter
-[![Travis-CI](https://travis-ci.com/SOF3/serde-iter.svg?branch=master)](https://travis-ci.com/SOF3/serde-iter)
+[![GitHub actions](https://github.com/SOF3/serde-iter/workflows/CI/badge.svg)](https://github.com/SOF3/serde-iter/actions?query=workflow%3ACI)
 [![crates.io](https://img.shields.io/crates/v/serde_iter.svg)](https://crates.io/crates/serde_iter)
 [![crates.io](https://img.shields.io/crates/d/serde_iter.svg)](https://crates.io/crates/serde_iter)
 [![docs.rs](https://docs.rs/serde_iter/badge.svg)](https://docs.rs/serde_iter)
+[![GitHub](https://img.shields.io/github/last-commit/SOF3/serde-iter)](https://github.com/SOF3/serde-iter)
 [![GitHub](https://img.shields.io/github/stars/SOF3/serde-iter?style=social)](https://github.com/SOF3/serde-iter)
 
 Iterator serializers for sequence/map
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # serde-iter
+//!
+//! [![GitHub actions](https://github.com/SOF3/serde-iter/workflows/CI/badge.svg)](https://github.com/SOF3/serde-iter/actions?query=workflow%3ACI)
+//! [![crates.io](https://img.shields.io/crates/v/serde_iter.svg)](https://crates.io/crates/serde_iter)
+//! [![crates.io](https://img.shields.io/crates/d/serde_iter.svg)](https://crates.io/crates/serde_iter)
+//! [![docs.rs](https://docs.rs/serde_iter/badge.svg)](https://docs.rs/serde_iter)
+//! [![GitHub](https://img.shields.io/github/last-commit/SOF3/serde-iter)](https://github.com/SOF3/serde-iter)
+//! [![GitHub](https://img.shields.io/github/stars/SOF3/serde-iter?style=social)](https://github.com/SOF3/serde-iter)
+//!
 //! This crate provides serializer functions to serialize iterator types as sequences and maps.
 //!
 //! See the documentation in each module for details.
